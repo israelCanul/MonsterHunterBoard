32ae5ed69fd2ba30f1cf88fecc50ddb8b04795e5
icanul-royalresorts <icanul@royalresorts.com>
2024-01-09 19:17:05 -0500

    ore render

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -14,18 +14,18 @@ function renderOre(ore = {}) {
   let oreRender = `
         <div class="col col-12 col-md-6 py-2">
           <div class="row bg-white">
-            <div class="col col-12 col-md-4 col-lg-3 monserrat">
-              ${ore}:
+            <div class="col col-12 col-md-6 col-lg-5 monserrat">
+              ${ore.label}:
             </div>
             <div
-              class="col col-12 col-md-8 col-lg-9 d-flex flex-row justify-content-between"
+              class="col col-12 col-md-6 col-lg-7 d-flex flex-row justify-content-between"
             >
               <span>1</span>
               <div class="actions d-flex flex-row gap-1">
-                <button type="button" class="btn btn-sm btn-outline-primary">
+                <button type="button" class="btn btn-sm btn-outline-primary" data-oreCode="${ore.value}">
                   add +
                 </button>
-                <button type="button" class="btn btn-sm btn-outline-primary">
+                <button type="button" class="btn btn-sm btn-outline-primary" data-oreCode="${ore.value}">
                   Use
                 </button>
               </div>
